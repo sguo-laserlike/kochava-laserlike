7d1bb26758865a7934934e0653701c25320171da
Steven Guo <sguo@laserlike.com>
2017-12-18 17:15:05 -0800

    - one more try

--- ios/RNKochavaLaserlike.h
+++ ios/RNKochavaLaserlike.h
@@ -5,11 +5,11 @@
 #import <React/RCTBridgeModule.h>
 #endif
 
-#import <React/RCTConvert.h>
-#import <React/RCTLog.h>
+// #import <React/RCTConvert.h>
+// #import <React/RCTLog.h>
 
-//#import "RCTEventEmitter.h"
-//#import "KochavaTracker.h"
+#import "RCTEventEmitter.h"
+#import "KochavaTracker.h"
 @interface RNKochavaLaserlike : RCTEventEmitter <RCTBridgeModule, KochavaTrackerDelegate>
 
 @property(nonatomic, strong) KochavaTracker* kochavaTracker;
